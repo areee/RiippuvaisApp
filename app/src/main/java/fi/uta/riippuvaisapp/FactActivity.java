59822934810abb0419058dedb93ba932ed3e8d54
areee <areee@users.noreply.github.com>
2017-04-18 19:54:05 +0300

    some code added

---
+++ app/src/main/java/fi/uta/riippuvaisapp/FactActivity.java
@@ -0,0 +1,13 @@
+package fi.uta.riippuvaisapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fact);
+    }
+}
--- app/src/main/java/fi/uta/riippuvaisapp/MainActivity.java
+++ app/src/main/java/fi/uta/riippuvaisapp/MainActivity.java
@@ -1,8 +1,14 @@
 package fi.uta.riippuvaisapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,25 +19,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // hampurilaisvalikko:
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // oikean alanurkan kalenterinappi:
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Tästä kalenteriin", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-
+        // lisää juttuja hampurilaisvalikkoon:
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,6 +52,7 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    // hampurilaisvalikon juttuja:
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -52,6 +63,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    // action bar -valikko:
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,6 +71,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // action bar -valikossa valittu kohteita:
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -67,27 +80,37 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_tietoja) {
+        if (id == R.id.action_about) {
+            System.out.println("Tietoja on painettu.");
+
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.app_name)
+                    .setMessage(R.string.about_app_message)
+                    .setIcon(R.drawable.information)
+                    .show();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    // hampurilaisvalikon kohteita valittu:
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            System.out.println("Kotiin valittu");
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_facts) {
+            System.out.println("Faktaa valittu");
 
-        } else if (id == R.id.nav_manage) {
-
+        } else if (id == R.id.nav_exercises) {
+            System.out.println("Tehtävät valittu");
+        } else if (id == R.id.nav_settings) {
+            System.out.println("Asetukset valittu");
         }
 
 //        else if (id == R.id.nav_share) {
