342a93e30f0894b87e100fd1955fb102f09d746f
areee <areee@users.noreply.github.com>
2017-05-02 11:47:21 +0300

    exercise 1 modified

--- app/src/main/java/fi/uta/riippuvaisapp/Exercise1Activity.java
+++ app/src/main/java/fi/uta/riippuvaisapp/Exercise1Activity.java
@@ -1,30 +1,116 @@
 package fi.uta.riippuvaisapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Exercise1Activity extends AppCompatActivity {
+    RadioGroup radioGroup1;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
+
+    RadioGroup radioGroup2;
+    RadioButton radioButton4;
+    RadioButton radioButton5;
+    RadioButton radioButton6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise1);
 
-        RadioGroup radioGroup1 = (RadioGroup) findViewById(R.id.exercise1_radioGroup1);
-        RadioButton radioButton1 = (RadioButton) findViewById(R.id.exercise1_radioButton1);
-        RadioButton radioButton2 = (RadioButton) findViewById(R.id.exercise1_radioButton2);
-        RadioButton radioButton3 = (RadioButton) findViewById(R.id.exercise1_radioButton3);
-        RadioGroup radioGroup2 = (RadioGroup) findViewById(R.id.exercise1_radioGroup2);
-        RadioButton radioButton4 = (RadioButton) findViewById(R.id.exercise1_radioButton4);
-        RadioButton radioButton5 = (RadioButton) findViewById(R.id.exercise1_radioButton5);
-        RadioButton radioButton6 = (RadioButton) findViewById(R.id.exercise1_radioButton6);
+        radioGroup1 = (RadioGroup) findViewById(R.id.exercise1_radioGroup1);
+        radioButton1 = (RadioButton) findViewById(R.id.exercise1_radioButton1);
+        radioButton2 = (RadioButton) findViewById(R.id.exercise1_radioButton2);
+        radioButton3 = (RadioButton) findViewById(R.id.exercise1_radioButton3);
+
+        radioGroup2 = (RadioGroup) findViewById(R.id.exercise1_radioGroup2);
+        radioButton4 = (RadioButton) findViewById(R.id.exercise1_radioButton4);
+        radioButton5 = (RadioButton) findViewById(R.id.exercise1_radioButton5);
+        radioButton6 = (RadioButton) findViewById(R.id.exercise1_radioButton6);
+
+        final TextView textView1 = (TextView) findViewById(R.id.seekBar_text_exercise1_1);
+        SeekBar seekBar1 = (SeekBar) findViewById(R.id.seekBar_exercise1_1);
+
+        final TextView textView2 = (TextView) findViewById(R.id.seekBar_text_exercise1_2);
+        SeekBar seekBar2 = (SeekBar) findViewById(R.id.seekBar_exercise1_2);
+
+        final TextView textView3 = (TextView) findViewById(R.id.seekBar_text_exercise1_3);
+        SeekBar seekBar3 = (SeekBar) findViewById(R.id.seekBar_exercise1_3);
+
+        final TextView textView4 = (TextView) findViewById(R.id.seekBar_text_exercise1_4);
+        SeekBar seekBar4 = (SeekBar) findViewById(R.id.seekBar_exercise1_4);
+
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView1.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView2.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView3.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        seekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView4.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
 
 //        Button read = (Button) findViewById(R.id.read_exercise1_button);
@@ -47,7 +133,57 @@ public class Exercise1Activity extends AppCompatActivity {
 
     public void checkAnswers(View v) {
 
+        int checkedRadioButtonId1 = radioGroup1.getCheckedRadioButtonId();
+        int checkedRadioButtonId2 = radioGroup2.getCheckedRadioButtonId();
 
+
+        int id1 = radioButton1.getId();
+        int id2 = radioButton2.getId();
+        int id3 = radioButton3.getId();
+
+        int id4 = radioButton4.getId();
+        int id5 = radioButton5.getId();
+        int id6 = radioButton6.getId();
+
+        if (checkedRadioButtonId1 == id2 && checkedRadioButtonId2 == id6) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Vastaukset oikein");
+            builder.setMessage("Molemmat vastaukset olivat oikein! Hyvä!");
+            builder.setPositiveButton("Jatka", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.show();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Vastauksessa korjattavaa");
+            builder.setMessage("Korjaa väärät vastaukset ja tarkista uudelleen.");
+            builder.setPositiveButton("Korjaa", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            builder.show();
+        }
+
+//        if (radioButton2.isSelected() && radioButton6.isSelected()) {
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setTitle("Vastaukset oikein");
+//            builder.setMessage("Molemmat vastaukset olivat oikein! Hyvä!");
+//            builder.setPositiveButton("Jatka", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    finish();
+//                }
+//            });
+//            builder.show();
+//        } else {
+//            System.out.println("Vastauksessa virheitä...");
+//        }
     }
+
 
 }
